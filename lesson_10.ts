c831e355a077a8d6ba7e61758871193991dc6a01
ex-vuvanthan001 <ex-vuvanthan001@ocft.com>
2026-07-27 10:45:25 +0700

    package init

---
+++ lesson_10.ts
@@ -0,0 +1,1 @@
+// ts la typescript
--- lesson_9_generic.ts
+++ lesson_9_generic.ts
@@ -1,7 +1,29 @@
 // generic type
-function identity(arg: number): number {
+function identity(arg : number): number {
   return arg;
 }
 
 console.log(identity(123));
 
+
+function identityG<T>(arg : T): T {
+  return arg;
+}
+
+console.log(identityG('123'));
+
+interface List<T>{
+  length:number;
+  [index:number]:T //mỗi phần tử là một giá trị T
+}
+
+const numberList: List<number> = [1,2,3]
+const wordList: List<string> = ["Hung","Than","Son"];
+
+interface Student {
+  id:number;
+  name:string
+}
+
+const studentL: List<Student> = [{id:1, name:"Than"}];
+console.log(studentL);
