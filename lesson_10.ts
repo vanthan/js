216e654cd803c632bff783d125cca53e5344cf19
ex-vuvanthan001 <ex-vuvanthan001@ocft.com>
2026-07-30 20:11:54 +0700

    lesson 10

--- lesson_10.ts
+++ lesson_10.ts
@@ -1,1 +1,18 @@
 // ts la typescript
+interface Student {
+    id:number,
+    name:string,
+    age:number,
+    gender:string
+}
+
+const bob = {
+    id:1,
+    name:'Bob',
+    age:12,
+    gender:'male'
+}
+
+const { id, name:newName} = bob; // là tạo ra hai biến id và name lấy giá trị từ bob
+                         // để đổi tên biến thì dùng name:newName
+
